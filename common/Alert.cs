69fddc9894eef9b1f61434412aef900e81f091ad
Polonit <louisdelavennedechoulot@gmail.com>
2024-03-01 01:16:54 +0100

    Alert Added And Product Improved

---
+++ common/Alert.cs
@@ -0,0 +1,19 @@
+﻿namespace RanceConnect;
+
+[Chroma.Name("Alerts")]
+public class Alert
+{
+    private string name;
+    private ushort type;
+    private string ean;
+    public string Name { get => name; set => name = value; }
+    public ushort Type { get => type; set => type = value; }
+    public string EAN { get => ean; set => ean = value; }
+
+    public Alert(string name, ushort type, string eAN)
+    {
+        Name = name;
+        Type = type;
+        EAN = eAN;
+    }
+}
